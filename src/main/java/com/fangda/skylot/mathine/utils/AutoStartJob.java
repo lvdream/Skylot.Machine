3e135b18c710f242b9324e3db603529a18c85049
lvdream <lvdream@gmail.com>
2018-02-04 23:28:41 +0800

    update code

--- src/main/java/com/fangda/skylot/mathine/utils/AutoStartJob.java
+++ src/main/java/com/fangda/skylot/mathine/utils/AutoStartJob.java
@@ -2,10 +2,12 @@ package com.fangda.skylot.mathine.utils;
 
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
 
 /**
  * 启动之后,自动执行一次的类,适用于Tomcat再次启动之后,同步的任务
  */
+@Component
 public class AutoStartJob implements
         ApplicationListener<ContextRefreshedEvent> {
 
