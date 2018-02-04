fe2de96f8e15a20d526db8fbaa06849e1d7c79df
lvdream <lvdream@gmail.com>
2018-02-04 23:30:09 +0800

    update code

--- src/main/java/com/fangda/skylot/mathine/utils/AutoStartJob.java
+++ src/main/java/com/fangda/skylot/mathine/utils/AutoStartJob.java
@@ -12,7 +12,9 @@ public class AutoStartJob implements
         ApplicationListener<ContextRefreshedEvent> {
 
     @Override
-    public void onApplicationEvent(ContextRefreshedEvent arg0) {
-        System.out.println("-----所有Bean载入完成---");
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        if (event.getApplicationContext().getParent() == null) {
+            System.out.println("-----所有Bean载入完成---");
+        }
     }
 }
